f4b7f45087671e7cb67e82764b0cdc9862a7215d
Tyler Burger <church940xp@gmail.com>
2017-04-03 23:14:44 -0700

    Working... For Now
    Still needs the test funcitons inserted

--- Gamma/Gamma.cpp
+++ Gamma/Gamma.cpp
@@ -32,12 +32,6 @@
 
 using namespace std;
 
-// Function Prototypes
-void welcome();
-vector<poi> create_pois(int point_num);
-vector<policy> create_pop(int pop_size, int point_num);
-vector<policy> replicate(vector<policy> population);
-
 // Classes
 class poi
 {
@@ -54,8 +48,16 @@ public:
 	double fitness;
 	void init(int point_num);
 	deque<int> mutate(deque<int> order, int number);
+	void evaluate(vector<poi> state);
 };
 
+// Function Prototypes
+void welcome();
+void print(vector<policy> population, vector<poi> state);
+vector<poi> create_pois(int point_num);
+vector<policy> create_pop(int pop_size, int point_num);
+vector<policy> replicate(vector<policy> population);
+vector<policy> bin_turney(vector<policy> population);
 
 //===========================================================================
 //===========================================================================					main
@@ -66,20 +68,31 @@ int main()
 
 	welcome();
 	int point_num = 10;		// Number of points of interest to visit
-	int generations = 10;	// Number of Generations
+	int generations = 1000;	// Number of Generations
 	int pop_size = 10;		// Half of total poulation size after replication
 
-	vector<poi> state;		// Stores each point of interest
-	state = create_pois(point_num);
+	vector<poi> state;					// Stores each point of interest
+	state = create_pois(point_num);		// Randomly create points of interest
 
-	vector<policy> population;
-	population = create_pop(pop_size, point_num);
-
+	vector<policy> population;						// Store each policy
+	population = create_pop(pop_size, point_num);	// Randomly create n-number of policies
+	
 	for (int i = 0; i < generations; i++)
 	{
-		population = replicate(population);
+		population = replicate(population);			// Perform replication and mutation
+		assert(pop_size * 2 == population.size());	// Check for proper poulation size
+
+		for (int j = 0; j < pop_size * 2; j++)
+		{
+			population.at(j).evaluate(state);		// Evaluate fitness of policies
+		}
+
+		population = bin_turney(population);		// Run binary turnament to downselect
+		assert(pop_size == population.size());		// Check for proper poulation size
+
 	}
 
+	print(population, state);
 
     return 0;
 }
@@ -87,7 +100,7 @@ int main()
 //===========================================================================					welcome
 void welcome()
 {
-
+	cout << "Running..." << endl;
 }
 
 //===========================================================================					create_pois
@@ -100,16 +113,15 @@ vector<poi> create_pois(int point_num)
 	for (int i = 0; i < point_num; i++)
 	{
 		poi A;
-		A.init(max_s, min_s);
-		city.push_back(A);
+		A.init(max_s, min_s);		// Send dimensions of the State to the initialization function
+		city.push_back(A);			// Add created location to the vector of cities
 	}
 	
-	// check to see that every pos in positions meets our expectations for initial dimensions
-	double temp[2];
+	// Check to see that every pos in positions meets our expectations for initial dimensions
 	for (int i = 0; i < point_num; i++) 
 	{
 		assert(city.at(i).x > min_s);
-		assert(city.at(i).x < min_s);
+		assert(city.at(i).x < max_s);
 		assert(city.at(i).y > min_s);
 		assert(city.at(i).y < max_s);
 	}
@@ -135,16 +147,133 @@ vector<policy> replicate(vector<policy> population)
 {
 	vector<policy> n_population;
 	n_population = population;
+	int num = 10;
+	int nu_2 = population.size();
+
 
 	while (n_population.size() < 2 * population.size())
 	{
 		int choice = cl4ptp*(n_population.size());
 		policy A = n_population.at(choice);
-		A.path = A.mutate(A.path, n_population.size / 10);
+		A.path = A.mutate(A.path, (nu_2 / num));
 		n_population.push_back(A);
 	}
-	assert(n_population.size() == population.size());
+	
+	assert(n_population.size() == 2*population.size());
 	return n_population;
+}
+
+//===========================================================================					bin_turney
+vector<policy> bin_turney(vector<policy> population)
+{
+	int end = population.size() / 2;
+	for (int i = 0; i < end; i++)
+	{
+		int choice_1 = cl4ptp*(population.size());
+		int choice_2 = cl4ptp*(population.size());
+		double fit_1;
+		double fit_2;
+		if (choice_1 != 0 && choice_2 != 0 && choice_1 != choice_2)
+		{
+			assert(choice_1 > 0);
+			int num = population.size();
+			if (choice_1 >= num)
+			{
+				choice_1 = choice_1 - (choice_1 - num + 1);
+			}
+			assert(choice_1 < num);
+
+			assert(choice_2 > 0);
+			if (choice_2 >= num)
+			{
+				choice_2 = choice_2 - (choice_2 - num + 1);
+			}
+			assert(choice_2 < num);
+
+			fit_1 = population.at(choice_1).fitness;
+			fit_2 = population.at(choice_2).fitness;
+
+			if (fit_1 > fit_2)
+			{
+				population.erase(population.begin() + choice_2);
+			}
+			else if (fit_1 < fit_2)
+			{
+				population.erase(population.begin() + choice_1);
+			}
+			else
+			{
+				population.erase(population.begin() + choice_2);
+			}
+		}
+		else
+		{
+			i--;
+		}
+	}
+	return population;
+}
+
+//===========================================================================					print
+void print(vector<policy> population, vector<poi> state)
+{
+	// Clear Console Screen
+	system("CLS");
+
+	// Now Writing Data To File
+	cout << "Now Writing Data To File" << endl;
+
+	// Create output file
+	ofstream output_file;
+	output_file.open("Salesman_Dist.txt");
+
+	int end = population.size();
+	for (int i = 0; i < end; i++)
+	{
+		output_file << i << '\t' << population.at(i).fitness << endl;
+	}
+
+	//Close output file
+	output_file.close();
+
+
+
+	// Create output file
+	output_file.open("Salesman_Path.txt");
+
+	// Find best route
+	double min_fit;
+	int pol_num;
+	min_fit = population.at(0).fitness;
+	pol_num = 0;
+	for (int i = 1; i < end; i++)
+	{
+		if (population.at(i).fitness < min_fit)
+		{
+			min_fit = population.at(i).fitness;
+			pol_num = i;
+		}
+	}
+
+	end = state.size();
+	for (int i = 0; i < end; i++)
+	{
+		int city_num;
+		city_num = population.at(pol_num).path.at(i);
+		output_file << state.at(city_num).x << '\t' << state.at(city_num).y << endl;
+	}
+
+	//Close output file
+	output_file.close();
+
+	// Wait for
+	Sleep(750);
+
+	// Clear Console Screen
+	system("CLS");
+
+	//User console update
+	cout << "Data Has Been Written To File" << endl;
 }
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++					poi::init
@@ -174,7 +303,12 @@ deque<int> policy::mutate(deque<int> order, int number)
 		if (choice != 0)
 		{
 			assert(choice > 0);
-			assert(choice < order.size());
+			int num = order.size();
+			if (choice >= num)
+			{
+				choice = choice - (choice - num + 1);
+			}
+			assert(choice < num);
 
 			int point = order.at(choice);
 			order.erase(order.begin() + choice);
@@ -184,7 +318,26 @@ deque<int> policy::mutate(deque<int> order, int number)
 		{
 			i--;
 		}
-		assert(order.at(0) = 0);
+		assert(order.at(0) == 0);
 	}
 	return order;
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++					policy::evaluate
+void policy::evaluate(vector<poi> state)
+{
+	fitness = 0;
+	int end = path.size();
+
+	double del_x;
+	double del_y;
+	double dist;
+	for (int i = 1; i < end; i++)
+	{
+		del_x = state.at(path.at(i)).x - state.at(path.at(i - 1)).x;
+		del_y = state.at(path.at(i)).y - state.at(path.at(i - 1)).y;
+		dist = del_x*del_x + del_y*del_y;
+		dist = sqrt(dist);
+		fitness = fitness + dist;
+	}
 }
