80fbf36c731ce0cef5269509860e9c9689842eae
Tyler Burger <church940xp@gmail.com>
2017-04-03 21:07:36 -0700

    Update 1

--- Gamma/Gamma.cpp
+++ Gamma/Gamma.cpp
@@ -31,17 +31,31 @@
 #include <Windows.h>
 
 using namespace std;
+
 // Function Prototypes
 void welcome();
-vector<double[2]> create_pois(int point_num);
+vector<poi> create_pois(int point_num);
+vector<policy> create_pop(int pop_size, int point_num);
+vector<policy> replicate(vector<policy> population);
 
-/*// Classes
+// Classes
+class poi
+{
+public:
+	double x;
+	double y;
+	void init(double max, double min);
+};
+
 class policy
 {
+public:
 	deque<int> path;
 	double fitness;
+	void init(int point_num);
+	deque<int> mutate(deque<int> order, int number);
 };
-*/
+
 
 //===========================================================================
 //===========================================================================					main
@@ -52,8 +66,20 @@ int main()
 
 	welcome();
 	int point_num = 10;		// Number of points of interest to visit
-	vector<double[2]> positions;	// Storage for off point locations
-	positions = create_pois(point_num);
+	int generations = 10;	// Number of Generations
+	int pop_size = 10;		// Half of total poulation size after replication
+
+	vector<poi> state;		// Stores each point of interest
+	state = create_pois(point_num);
+
+	vector<policy> population;
+	population = create_pop(pop_size, point_num);
+
+	for (int i = 0; i < generations; i++)
+	{
+		population = replicate(population);
+	}
+
 
     return 0;
 }
@@ -65,31 +91,100 @@ void welcome()
 }
 
 //===========================================================================					create_pois
-vector<double[2]> create_pois(int point_num)
+vector<poi> create_pois(int point_num)
 {
-	double min = 0;
-	double max = 100;
-	double pos[2];
-	vector<double[2]> positions;
+	double min_s = 0;
+	double max_s = 100;
+	vector<poi> city;
 
 	for (int i = 0; i < point_num; i++)
 	{
-		pos[0] = cl4ptp*(max - min) + min;
-		pos[1] = cl4ptp*(max - min) + min;
-		positions.push_back(pos);
+		poi A;
+		A.init(max_s, min_s);
+		city.push_back(A);
 	}
 	
 	// check to see that every pos in positions meets our expectations for initial dimensions
 	double temp[2];
 	for (int i = 0; i < point_num; i++) 
 	{
-		temp[0] = positions[i][0];
-		temp[1] = positions[i][1];
-		assert(temp[0] > min);
-		assert(temp[0] < max);
-		assert(temp[1] > min);
-		assert(temp[1] < max);
+		assert(city.at(i).x > min_s);
+		assert(city.at(i).x < min_s);
+		assert(city.at(i).y > min_s);
+		assert(city.at(i).y < max_s);
 	}
+	return city;
 }
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++					pois::init
+//===========================================================================					create_pop
+vector<policy> create_pop(int pop_size, int point_num)
+{
+	vector<policy> pop;
+
+	for (int i = 0; i < pop_size; i++)
+	{
+		policy A;
+		A.init(point_num);
+		pop.push_back(A);
+	}
+	return pop;
+}
+
+//===========================================================================					replicate
+vector<policy> replicate(vector<policy> population)
+{
+	vector<policy> n_population;
+	n_population = population;
+
+	while (n_population.size() < 2 * population.size())
+	{
+		int choice = cl4ptp*(n_population.size());
+		policy A = n_population.at(choice);
+		A.path = A.mutate(A.path, n_population.size / 10);
+		n_population.push_back(A);
+	}
+	assert(n_population.size() == population.size());
+	return n_population;
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++					poi::init
+void poi::init(double max, double min)
+{
+	x = cl4ptp*(max - min) + min;
+	y = cl4ptp*(max - min) + min;
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++					policy::init
+void policy::init(int point_num)
+{
+	deque<int> ordered_poi;
+	for (int i = 0; i < point_num; i++)
+	{
+		ordered_poi.push_back(i);
+	}
+
+	path = mutate(ordered_poi, point_num/2);
+}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++					policy::mutate
+deque<int> policy::mutate(deque<int> order, int number)
+{
+	for (int i = 0; i < number; i++)
+	{
+		int choice = cl4ptp*(order.size());
+		if (choice != 0)
+		{
+			assert(choice > 0);
+			assert(choice < order.size());
+
+			int point = order.at(choice);
+			order.erase(order.begin() + choice);
+			order.push_back(point);
+		}
+		else
+		{
+			i--;
+		}
+		assert(order.at(0) = 0);
+	}
+	return order;
+}
